26b2bbae7e5ce17f1e205c27922f0a02d66fd5f4
Jack Tanner <jack@jackandtheblockstalk.com>
2022-09-23 16:32:52 +0200

    Sucessfully implemented linked packages with an Ethereum VC

---
+++ test/credential.test.ts
@@ -0,0 +1,37 @@
+// Fix for https://stackoverflow.com/questions/68468203/why-am-i-getting-textencoder-is-not-defined-in-jest
+import { TextEncoder, TextDecoder } from 'util'
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder as any
+
+import { EthrDID } from 'ethr-did'
+import { Issuer } from 'did-jwt-vc'
+import { JwtCredentialPayload, createVerifiableCredentialJwt } from 'did-jwt-vc'
+
+describe('Issue and verify credential', () => {
+
+    it('Issues an Ethereum credential, testing backwards compatibility', async () => {
+
+        const issuer = new EthrDID({
+            identifier: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
+            privateKey: 'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
+          }) as Issuer
+          
+          const vcPayload: JwtCredentialPayload = {
+            sub: 'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
+            nbf: 1562950282,
+            vc: {
+              '@context': ['https://www.w3.org/2018/credentials/v1'],
+              type: ['VerifiableCredential'],
+              credentialSubject: {
+                degree: {
+                  type: 'BachelorDegree',
+                  name: 'Baccalauréat en musiques numériques'
+                }
+              }
+            }
+          }
+          
+          const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer)
+          expect(vcJwt).toBe("eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImRlZ3JlZSI6eyJ0eXBlIjoiQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQmFjY2FsYXVyw6lhdCBlbiBtdXNpcXVlcyBudW3DqXJpcXVlcyJ9fX0sInN1YiI6ImRpZDpldGhyOjB4NDM1ZGYzZWRhNTcxNTRjZjhjZjc5MjYwNzk4ODFmMjkxMmY1NGRiNCIsIm5iZiI6MTU2Mjk1MDI4MiwiaXNzIjoiZGlkOmV0aHI6MHhGMTIzMkY4NDBmM2FEN2QyM0ZjRGFBODRkNkM2NmRhYzI0RUZiMTk4In0.BO15QQyc7pQKTxGy7OTp-ZUIsNBkNFkBnVJmFTcsltc3vSP10qVT57IiURDSb6onaioS7Bd499K8yp83OYx4gwA");
+    })
+})
