3824c8d9b23b99c8fd6e2e66335528785f0e68b4
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-04 12:57:03 +0100

    hotfix issue with did-jwt-vc package import name

--- test/util/vc.ts
+++ test/util/vc.ts
@@ -1,18 +1,18 @@
-import { JwtCredentialPayload } from "did-jwt-vc";
+import { JwtCredentialPayload } from "@tonomy/did-jwt-vc";
 
 export const did = 'did:antelope:eos:testnet:jungle:reball1block';
 
 export const vcPayload: JwtCredentialPayload = {
-    sub: did,
-    nbf: 1562950282,
-    vc: {
-      '@context': ['https://www.w3.org/2018/credentials/v1'],
-      type: ['VerifiableCredential'],
-      credentialSubject: {
-        degree: {
-          type: 'BachelorDegree',
-          name: 'Baccalauréat en musiques numériques',
-        },
+  sub: did,
+  nbf: 1562950282,
+  vc: {
+    '@context': ['https://www.w3.org/2018/credentials/v1'],
+    type: ['VerifiableCredential'],
+    credentialSubject: {
+      degree: {
+        type: 'BachelorDegree',
+        name: 'Baccalauréat en musiques numériques',
       },
     },
-  };
+  },
+};
